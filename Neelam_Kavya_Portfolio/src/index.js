b94d32acfa183f1b7962da789bbd638e16fe7287
Kavya Neelam <kavyaneelam9999@gmail.com>
2025-09-29 21:28:36 -0500

    Update index.js

--- Neelam_Kavya_Portfolio/src/index.js
+++ Neelam_Kavya_Portfolio/src/index.js
@@ -4,6 +4,7 @@ import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import data from './portfolio_shared_data.json';
 
 /* GLOBAL VARIABLES */
 
